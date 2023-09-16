2343078282733a990b61fa4c4d3bb43806b5a664
Your Name <youremail@example.com>
2023-09-16 21:23:45 +0600

    hulu website

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector(".modal");
+const loginButton = document.querySelector(".login-btn");
+const closeBtn = document.querySelector(".close");
+
+loginButton.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+
+window.addEventListener("click", outsideClick);
+
+function openModal() {
+  modal.style.display = "block";
+}
+function closeModal() {
+  modal.style.display = "none";
+}
+function outsideClick(e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+}
